2662c2b7edad93a2e8a5dc65c56f094453956749
GZTan23 <zhong.tanguanzhong@hotmail.com>
2025-03-25 14:22:58 +0800

    Add edit_lesson command

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -16,6 +16,7 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "The student index provided is invalid!";
+    public static final String MESSAGE_INVALID_LESSON_DISPLAYED_INDEX = "The lesson index provided is invalid!";
     public static final String MESSAGE_STUDENTS_LISTED_OVERVIEW = "%1$d students listed!";
     public static final String MESSAGE_LESSONS_LISTED_OVERVIEW = "%1$d lessons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
@@ -57,7 +58,7 @@ public class Messages {
     public static String format(Lesson lesson) {
         final StringBuilder builder = new StringBuilder();
         builder.append(lesson.getSubjects())
-                .append(" lesson; Student Name:")
+                .append(" lesson; Student Name: ")
                 .append(lesson.getName())
                 .append("; Date: ")
                 .append(lesson.getDate())
---
+++ src/main/java/seedu/address/logic/parser/EditLessonCommandParser.java
@@ -0,0 +1,84 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.EditLessonCommand;
+import seedu.address.logic.commands.EditLessonCommand.EditLessonDescriptor;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.subject.Subject;
+
+/**
+ * Parses input arguments and creates a new EditLessonCommand object
+ */
+public class EditLessonCommandParser implements Parser<EditLessonCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the EditLessonCommand
+     * and returns an EditLessonCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public EditLessonCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_SUBJECT, PREFIX_NAME, PREFIX_DATE, PREFIX_TIME);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    EditLessonCommand.MESSAGE_USAGE), pe);
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_DATE, PREFIX_TIME, PREFIX_SUBJECT);
+
+        EditLessonDescriptor editLessonDescriptor = new EditLessonDescriptor();
+
+        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+            editLessonDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
+        }
+        if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            editLessonDescriptor.setDate(ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_TIME).isPresent()) {
+            editLessonDescriptor.setTime(ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME).get()));
+        }
+
+        parseSubjectsForEdit(argMultimap.getAllValues(PREFIX_SUBJECT)).ifPresent(editLessonDescriptor::setSubjects);
+
+        if (!editLessonDescriptor.isAnyFieldEdited()) {
+            throw new ParseException(EditLessonCommand.MESSAGE_NOT_EDITED);
+        }
+
+        return new EditLessonCommand(index, editLessonDescriptor);
+    }
+
+    /**
+     * Parses {@code Collection<String> subjects} into a {@code Set<Subject>} if {@code subjects} is non-empty.
+     * If {@code subjects} contain only one element which is an empty string, it will be parsed into a
+     * {@code Set<Subject>} containing zero subjects.
+     */
+    private Optional<Set<Subject>> parseSubjectsForEdit(Collection<String> subjects) throws ParseException {
+        assert subjects != null;
+
+        if (subjects.isEmpty()) {
+            return Optional.empty();
+        }
+        Collection<String> subjectSet = subjects.size() == 1 && subjects.contains("") ? Collections.emptySet()
+                : subjects;
+        return Optional.of(ParserUtil.parseSubjects(subjectSet));
+    }
+
+}
