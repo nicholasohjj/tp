691d4d9d6ba0a56b1b97b2f746b92121a1fcbcda
GZTan23 <zhong.tanguanzhong@hotmail.com>
2025-03-25 18:35:13 +0800

    Fix tests for list_lessons

--- src/test/java/seedu/address/logic/parser/ListLessonsCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListLessonsCommandParserTest.java
@@ -3,32 +3,34 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-
-import java.util.Arrays;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_LESSONS;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.model.student.NameContainsKeywordsPredicate;
+import seedu.address.logic.commands.ListLessonsCommand;
+import seedu.address.model.lesson.StudentNameLessonPredicate;
 
 public class ListLessonsCommandParserTest {
 
-    private FindCommandParser parser = new FindCommandParser();
+    private ListLessonsCommandParser parser = new ListLessonsCommandParser();
 
     @Test
-    public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    public void parse_emptyArg_showsAllLessons() {
+        ListLessonsCommand expectedFindCommand =
+                new ListLessonsCommand(PREDICATE_SHOW_ALL_LESSONS);
+        assertParseSuccess(parser, "     ", expectedFindCommand);
     }
 
     @Test
     public void parse_validArgs_returnsFindCommand() {
-        // no leading and trailing whitespaces
-        FindCommand expectedFindCommand =
-                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
-        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+        // leading and trailing whitespaces
+        ListLessonsCommand expectedFindCommand =
+                new ListLessonsCommand(new StudentNameLessonPredicate("Alice"));
+        assertParseSuccess(parser, " n/ Alice ", expectedFindCommand);
 
-        // multiple whitespaces between keywords
-        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+        // blank keywords are not allowed
+        assertParseFailure(parser, " n/ ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ListLessonsCommand.MESSAGE_USAGE));
     }
 
 }
