251979e94cb7e5e84718c59659eab3097034ea29
nicholasohjj <nicholasohjj@gmail.com>
2025-03-27 11:30:58 +0800

    Add logging to commands

--- src/main/java/seedu/address/logic/commands/FindStudentCommand.java
+++ src/main/java/seedu/address/logic/commands/FindStudentCommand.java
@@ -2,6 +2,9 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
@@ -14,6 +17,8 @@ import seedu.address.model.student.NameContainsKeywordsPredicate;
 public class FindStudentCommand extends Command {
 
     public static final String COMMAND_WORD = "find_student";
+    private static final Logger logger = Logger.getLogger(FindStudentCommand.class.getName());
+
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all students whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
@@ -22,14 +27,23 @@ public class FindStudentCommand extends Command {
 
     private final NameContainsKeywordsPredicate predicate;
 
+    /**
+     * Constructs new FindStudentCommand with given predicate
+     */
     public FindStudentCommand(NameContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
+        logger.log(Level.FINE, "FindStudentCommand created with predicate: {0}", predicate);
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        logger.log(Level.INFO, "Executing FindStudentCommand with predicate: {0}", predicate);
+
         model.updateFilteredStudentList(predicate);
+        int filteredListSize = model.getFilteredStudentList().size();
+
+        logger.log(Level.INFO, "FindStudentCommand found {0} students matching the criteria", filteredListSize);
         return new CommandResult(
                 String.format(Messages.MESSAGE_STUDENTS_LISTED_OVERVIEW, model.getFilteredStudentList().size()), true);
     }
