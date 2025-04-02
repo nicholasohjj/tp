4c029f91fe9ac53605e1c852358a2e6b970a5f20
nicholasohjj <nicholasohjj@gmail.com>
2025-04-02 20:30:40 +0800

    Add logging for parser classes

--- src/main/java/seedu/address/logic/parser/EditLessonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditLessonCommandParser.java
@@ -7,6 +7,9 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
 
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditLessonCommand;
 import seedu.address.logic.commands.EditLessonCommand.EditLessonDescriptor;
@@ -16,6 +19,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
  * Parses input arguments and creates a new EditLessonCommand object
  */
 public class EditLessonCommandParser implements Parser<EditLessonCommand> {
+    private static final Logger logger = LogsCenter.getLogger(EditLessonCommandParser.class);
 
     /**
      * Parses the given {@code String} of arguments in the context of the EditLessonCommand
@@ -24,35 +28,62 @@ public class EditLessonCommandParser implements Parser<EditLessonCommand> {
      */
     public EditLessonCommand parse(String args) throws ParseException {
         requireNonNull(args);
+        logger.info("Parsing EditLessonCommand with arguments: " + args);
+
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_SUBJECT, PREFIX_NAME, PREFIX_DATE, PREFIX_TIME);
 
         if (argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditLessonCommand.MESSAGE_USAGE));
+            logger.warning("Empty preamble in EditLessonCommand");
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    EditLessonCommand.MESSAGE_USAGE));
         }
 
-        Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_DATE, PREFIX_TIME, PREFIX_SUBJECT);
+        try {
+            Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            logger.fine("Parsed lesson index: " + index.getOneBased());
 
-        EditLessonDescriptor editLessonDescriptor = new EditLessonDescriptor();
+            argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_DATE, PREFIX_TIME, PREFIX_SUBJECT);
 
+            EditLessonDescriptor editLessonDescriptor = new EditLessonDescriptor();
+            parseFields(argMultimap, editLessonDescriptor);
+
+            if (!editLessonDescriptor.isAnyFieldEdited()) {
+                logger.warning("No fields edited in EditLessonCommand");
+                throw new ParseException(EditLessonCommand.MESSAGE_NOT_EDITED);
+            }
+
+            logger.info("Successfully parsed EditLessonCommand for lesson index: " + index.getOneBased());
+            return new EditLessonCommand(index, editLessonDescriptor);
+        } catch (ParseException pe) {
+            logger.warning("Error parsing EditLessonCommand: " + pe.getMessage());
+            throw pe;
+        } catch (Exception e) {
+            logger.severe("Unexpected error parsing EditLessonCommand: " + e.getMessage());
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    EditLessonCommand.MESSAGE_USAGE));
+        }
+    }
+
+    /**
+     * Parses individual fields from the argument multimap into the edit descriptor.
+     */
+    private void parseFields(ArgumentMultimap argMultimap, EditLessonDescriptor descriptor) throws ParseException {
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            editLessonDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
+            descriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
+            logger.fine("Parsed name field");
         }
         if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
-            editLessonDescriptor.setDate(ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()));
+            descriptor.setDate(ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()));
+            logger.fine("Parsed date field");
         }
         if (argMultimap.getValue(PREFIX_TIME).isPresent()) {
-            editLessonDescriptor.setTime(ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME).get()));
+            descriptor.setTime(ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME).get()));
+            logger.fine("Parsed time field");
         }
         if (argMultimap.getValue(PREFIX_SUBJECT).isPresent()) {
-            editLessonDescriptor.setSubject(ParserUtil.parseSubject(argMultimap.getValue(PREFIX_SUBJECT).get()));
+            descriptor.setSubject(ParserUtil.parseSubject(argMultimap.getValue(PREFIX_SUBJECT).get()));
+            logger.fine("Parsed subject field");
         }
-
-        if (!editLessonDescriptor.isAnyFieldEdited()) {
-            throw new ParseException(EditLessonCommand.MESSAGE_NOT_EDITED);
-        }
-
-        return new EditLessonCommand(index, editLessonDescriptor);
     }
 }
--- src/main/java/seedu/address/logic/parser/FindStudentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindStudentCommandParser.java
@@ -3,7 +3,9 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.FindStudentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.student.NameContainsKeywordsPredicate;
@@ -12,6 +14,7 @@ import seedu.address.model.student.NameContainsKeywordsPredicate;
  * Parses input arguments and creates a new FindStudentCommand object
  */
 public class FindStudentCommandParser implements Parser<FindStudentCommand> {
+    private static final Logger logger = LogsCenter.getLogger(FindStudentCommandParser.class);
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindStudentCommand
@@ -19,15 +22,32 @@ public class FindStudentCommandParser implements Parser<FindStudentCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindStudentCommand parse(String args) throws ParseException {
+        assert args != null : "Input arguments string cannot be null";
+        logger.info("Parsing FindStudentCommand with arguments: " + args);
+
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
+            logger.warning("Empty arguments provided to FindStudentCommand");
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindStudentCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        try {
+            String[] nameKeywords = trimmedArgs.split("\\s+");
+            assert nameKeywords != null && nameKeywords.length > 0 : "Keywords array cannot be null or empty";
 
-        return new FindStudentCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+            logger.info("Successfully parsed FindStudentCommand with keywords: "
+                    + Arrays.toString(nameKeywords));
+            return new FindStudentCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        } catch (NullPointerException npe) {
+            logger.severe("Null value encountered while parsing FindStudentCommand");
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    FindStudentCommand.MESSAGE_USAGE));
+        } catch (Exception e) {
+            logger.warning("Unexpected error parsing FindStudentCommand: " + e.getMessage());
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    FindStudentCommand.MESSAGE_USAGE));
+        }
     }
 
 }
