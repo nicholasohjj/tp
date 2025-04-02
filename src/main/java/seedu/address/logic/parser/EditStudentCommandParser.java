db0de4bb45a310791051a52289fe3ab919c3cdb8
nicholasohjj <nicholasohjj@gmail.com>
2025-04-02 22:31:25 +0800

    replace EditCommmand with EditStudentCommand

--- src/main/java/seedu/address/logic/parser/EditStudentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditStudentCommandParser.java
@@ -16,14 +16,14 @@ import seedu.address.logic.commands.EditStudentCommand.EditStudentDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new EditCommand object
+ * Parses input arguments and creates a new EditStudentCommand object
  */
 public class EditStudentCommandParser implements Parser<EditStudentCommand> {
     private static final Logger logger = LogsCenter.getLogger(EditStudentCommandParser.class);
 
     /**
-     * Parses the given {@code String} of arguments in the context of the EditCommand
-     * and returns an EditCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the EditStudentCommand
+     * and returns an EditStudentCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public EditStudentCommand parse(String args) throws ParseException {
