021892b441dc280488b03f136db6942592cf5cc6
nicholasohjj <nicholasohjj@gmail.com>
2025-04-02 16:03:24 +0800

    Add logging and assertions to list commands

--- src/main/java/seedu/address/logic/commands/ListStudentsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListStudentsCommand.java
@@ -3,6 +3,9 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_STUDENTS;
 
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.Model;
 
 /**
@@ -14,14 +17,27 @@ public class ListStudentsCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all students";
     public static final String MESSAGE_EMPTY_LIST = "No students found";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Lists all students in the address book.\n"
+            + "Example: " + COMMAND_WORD;
 
+    private static final Logger logger = LogsCenter.getLogger(ListStudentsCommand.class);
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        logger.info("Executing ListStudentsCommand");
+
         model.updateFilteredStudentList(PREDICATE_SHOW_ALL_STUDENTS);
+        int studentCount = model.getFilteredStudentList().size();
+
+        if (studentCount == 0) {
+            logger.info("Address book is empty - no students found");
+            return new CommandResult(MESSAGE_EMPTY_LIST, true);
+        }
+
+        logger.info("Listed " + studentCount + " students");
         return new CommandResult(
-                model.getFilteredStudentList().isEmpty()
-                ? MESSAGE_EMPTY_LIST
-                : MESSAGE_SUCCESS, true);
+                String.format(MESSAGE_SUCCESS, studentCount),
+                true);
     }
 }
