0f7846590ee4ea6eaa5926eca038a56e16e38b38
nicholasohjj <nicholasohjj@gmail.com>
2025-04-02 21:13:08 +0800

    Add logging for parser classes

--- src/test/java/seedu/address/logic/parser/DeleteLessonCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteLessonCommandParserTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 
 import org.junit.jupiter.api.Test;
@@ -28,12 +27,14 @@ public class DeleteLessonCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteLessonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteLessonCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_zeroIndex_throwsParseException() {
-        assertParseFailure(parser, "0", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteLessonCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "0", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteLessonCommand.MESSAGE_USAGE));
     }
 
     @Test
